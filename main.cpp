7474be04d297800538a2439a1e36f97afa74d15d
Noah <78211701+thanknoah@users.noreply.github.com>
2025-10-25 20:34:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,21 +9,51 @@
 #include <chrono>
 #include <utility>
 #include <future>
+#include <random>
+#include <variant>
 
 class threadPool {
 private:
-    struct info {
+    struct threadUsageInfo {
+        int threadId;
+        std::shared_ptr<std::atomic<int>> threadTaskAmounts;
+    };
+    struct threadTaskInfo {
         std::queue<std::function<void()>> taskQuene; // Stores the tasks 
         std::mutex queneMutex; // stop inserting while internalThreadOperations reads quene
     };
-    std::unordered_map<int, std::shared_ptr<info>> threadInformation; // never changes size so no need for mutex
-    std::vector<std::thread> threadList;
-    std::atomic<int> optimalThread;
 
-    void internalThreadOperation(std::shared_ptr<info>& e) {
+    std::unordered_map<int, std::shared_ptr<threadTaskInfo>> threadInformation; // never changes size so no need for mutex
+    std::vector<threadUsageInfo> threadUsage;
+    std::vector<std::thread> threadList;
+    std::atomic<bool> stopThread;
+
+    int findLeastExhaustedWorker() {
+        if (threadUsage.empty()) return -1; // no threads
+
+        auto it = threadUsage.begin();
+        int smallestWorkerSize = it->threadId;
+        int smallestNum = it->threadTaskAmounts->load();
+
+        for (; it != threadUsage.end(); ++it) {
+            int currentThreadTasksAmount = it->threadTaskAmounts->load();
+
+            if (currentThreadTasksAmount < smallestNum) {
+                smallestWorkerSize = it->threadId;
+                smallestNum = currentThreadTasksAmount;
+            }
+        }
+        return smallestWorkerSize;
+    }
+
+    void internalThreadOperation(std::shared_ptr<threadTaskInfo> e, std::shared_ptr<std::atomic<int>> taskMonitoring) {
         while (true) {
-            if (e->taskQuene.empty()) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(10)); continue;
+            if (e->taskQuene.empty() && !stopThread.load()) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(50));
+                continue;
+            }
+            else if (e->taskQuene.empty() && stopThread.load()) {
+                break;
             }
 
             std::function<void()> task;
@@ -31,34 +61,28 @@ private:
                 std::lock_guard<std::mutex> queneMutexLock(e->queneMutex);
                 task = std::move(e->taskQuene.front());
                 e->taskQuene.pop();
+                (*taskMonitoring)--;
             }
             task();
         }
     }
 
+    void monitorTasksPerThread(int waitMs) {
+        auto waitTime = std::chrono::milliseconds(waitMs);
 
-
-    void findLeastExhaustedWorker() {
         while (true) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            int smallestNum = 0;
-            int smallestWorkerSize = 0;
+            std::this_thread::sleep_for(waitTime);
 
-            for (auto it = threadInformation.begin(); it != threadInformation.end(); ++it) {
-                std::unique_lock<std::mutex> templock(it->second->queneMutex);
-
-                if (it->second->taskQuene.size() <= smallestNum) {
-                    smallestWorkerSize = it->first;
-                    smallestNum = it->second->taskQuene.size();
-                }
+            for (auto it = threadUsage.begin(); it != threadUsage.end(); ++it) {
+                std::cout << "Thread #" << it->threadId << ": " << it->threadTaskAmounts->load() << " Tasks \n ";
             }
-            optimalThread.store(smallestWorkerSize);
+            std::cout << "\n";
         }
     }
 
-
 public:
     ~threadPool() {
+        stopThread.store(true);
         for (auto& thread : threadList) {
             if (thread.joinable()) {
                 thread.join();
@@ -67,48 +91,101 @@ public:
     }
 
     void init() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        threadUsage.resize(std::thread::hardware_concurrency());
+        stopThread.store(false);
+     
         for (int x = 0; x < std::thread::hardware_concurrency(); ++x) {
-            auto internalThreadDetails = std::make_shared<info>();
-            std::thread t(std::bind(&threadPool::internalThreadOperation, this, internalThreadDetails));
-            threadList.push_back(std::move(t));
+            auto internalThreadDetails = std::make_shared<threadTaskInfo>();
+            auto internalTaskRef = std::make_shared<std::atomic<int>>();
+
+            threadUsage[x].threadId = x;
+            threadUsage[x].threadTaskAmounts = internalTaskRef;
+
+            std::thread t([this, internalThreadDetails, internalTaskRef]() {
+                internalThreadOperation(internalThreadDetails, internalTaskRef);
+            });
+
             threadInformation.insert({ x, internalThreadDetails });
+            threadList.push_back(std::move(t));
         }
 
-        std::thread t1(std::bind(&threadPool::findLeastExhaustedWorker, this));
-        threadList.push_back(std::move(t1));
+        //threadList.emplace_back([this]() { monitorTasksPerThread(1000); });
     };
 
     template <typename F, typename... Args>
     auto executeTaskInThreadPool(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>> { // Telling compiler return result is std::future<> and wtv value of function return value is
-        auto threadToExecute = optimalThread.load();
-        using return_type = std::invoke_result_t<F, Args...>; // Gets value type of typenmae F and Args at compile time
-        std::future<return_type>futureValue;
+        auto threadToExecute = findLeastExhaustedWorker();
+        using return_type = std::invoke_result_t<F, Args...>; // Getting return type of function
+        std::future<return_type> futureResult;
 
-        auto func = std::make_shared<std::packaged_task<return_type()>>( // Turns function into an std::packaged_task, so user can use future to aysncronously retrieve result 
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        threadInformation[threadToExecute]->taskQuene.emplace([func]() { (*func)(); }); // place function into threads task quene
+        if constexpr (std::is_void_v<return_type>) {
+            auto task = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+            {
+                std::lock_guard<std::mutex> lock(threadInformation[threadToExecute]->queneMutex);
+                threadInformation[threadToExecute]->taskQuene.push(task);
+            }
+            (*threadUsage[threadToExecute].threadTaskAmounts)++;
+            return futureResult;
 
-        futureValue = func->get_future();
-        return futureValue;
+        }
+        else {
+            auto task = std::make_shared<std::packaged_task<return_type()>>( // Putitng it in a packaged task so can retrieve reuslts aysncronously
+                std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+            );
+            futureResult = task->get_future();
+
+            {
+                std::lock_guard<std::mutex> lock(threadInformation[threadToExecute]->queneMutex);
+                threadInformation[threadToExecute]->taskQuene.push([task]() {
+                    (*task)();
+                });
+            }
+
+            (*threadUsage[threadToExecute].threadTaskAmounts)++;
+            return futureResult;
+        }
     }
 };
 
-int add(int a, int b) {
+
+// demonstration functions
+void add(int a, int b) {
+    a + b;
+}
+
+int returnAdd(int a, int b) {
     return a + b;
 }
 
-// demo
 int main() {
+    // Intialise it
     threadPool t;
     t.init();
 
+    // Submit tasks without retrieving results
+    for (size_t i = 0; i < 10; ++i) {
+        t.executeTaskInThreadPool(add, 1, 2);
+    }
 
-    auto calculation = t.executeTaskInThreadPool(add, 1, 2);
-    std::cout << "Non blocking\n"; 
-    auto calculation2 = t.executeTaskInThreadPool(add, 3, 5);
-    auto calculation3 = t.executeTaskInThreadPool(add, 5, 6);
-    std::cout << calculation.get() << "\n";
-    std::cout << calculation2.get() << "\n";
-    std::cout << calculation3.get() << "\n";
+    // Can do work on main thread
+    std::cout << "All tasks submitted (non returning, non-blocking). \n";
+
+    // Submit Tasks with getting results
+    std::vector<std::future<int>> calculationResults; // storing results as futures
+    for (size_t x = 0; x < 10; ++x) {
+        calculationResults.emplace_back(t.executeTaskInThreadPool(returnAdd, x, x)); 
+    }
+
+    // Can do work on main thread
+    std::cout << "All tasks submitted (returning, non-blocking).\n";
+
+    // Retrieving results for all tasks
+    for (size_t x = 0; x < 10; ++x) {
+        int result = calculationResults[x].get();
+        std::cout << "Result of task #" << x << ": " << result << "\n";
+    }
+
+
 }
+
